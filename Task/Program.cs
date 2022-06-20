0f6ebead5571247d440bd2bb288edd713556c3bb
Daria <daria.kulepova@gmail.com>
2022-06-20 08:05:24 +0300

    Задали два массива одной длины

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+string[] array1 = new string[] { "WowWow", "s2", "New", "Moscow", "5" };
+string[] array2 = new string[array1.Length];
+
