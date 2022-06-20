2e6df2ec3e813308d7739365495980eac146ce6e
Daria <daria.kulepova@gmail.com>
2022-06-20 08:08:12 +0300

    Добавили метод заполнения первого массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,3 +4,18 @@
 string[] array1 = new string[] { "WowWow", "s2", "New", "Moscow", "5" };
 string[] array2 = new string[array1.Length];
 
+PrintArray(array1);
+
+
+
+
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
