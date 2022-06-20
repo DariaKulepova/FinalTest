5e00a83460f949a2d400fe9db0614f23f010067c
Daria <daria.kulepova@gmail.com>
2022-06-20 08:10:28 +0300

    Добавили метод заполнения второго массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,11 +5,8 @@ string[] array1 = new string[] { "WowWow", "s2", "New", "Moscow", "5" };
 string[] array2 = new string[array1.Length];
 
 PrintArray(array1);
-
-
-
-
-
+SelectionSort(array1, array2);
+PrintArray(array2);
 
 void PrintArray(string[] array)
 {
@@ -19,3 +16,23 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
+ PrintArray(array1);
+
+
+
+
+void SelectionSort(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+
+        }
+    }
+    Console.WriteLine(" ", array2);
+}
+
